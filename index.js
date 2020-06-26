c2989bd73753153d2cf2573912d0b6cced362534
zhuchunyuan <719286213@qq.com>
2020-06-26 20:39:25 +0800

    更新index.js

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 console.log('prod is ok')
 
 alert('你好啊！')
+// git init   初始化git本地仓
+// git add     追踪文件、把修改过后的文件添加到暂存区、处理冲突后添加暂存区
+// git add .
+// git commit -m '描述信息'   把文件添加git仓库
+// git commit -a -m '描述信息'  一次性把已追踪或修改过的文件放到git仓库
+// git status -s   查看本地文件的状态
+// git status 
+// git push -u origin 分支名    第一次把本地分支push到云端
+// git push      非第一次push到云端
+// git branch     查看本地分支
+// git branch 分支名    创建新的分支
+// git branch -d 分支名    删除分支
+// git branch -D 分支名    强制删除分支
+// git checkout 分支名     切换分支
+// git checkout -b 分支名    创建并切换到此分支
+// git pull     从云端取到该分支
+// git merge 分支名    合并分支
+// git remote show origin    查看远程分支
+// git remote add origin https://github.com/zhuchunyuan196216/ceshi.git    链接云端和本地仓库HTTPS方式
+// git push -u origin master   主分支push到云端上
+// git remote add origin git@github.com:zhuchunyuan196216/ceshi.git    ssh方式
+// git push -u origin master
+// git stash   缓存代码
+// git stash pop   取出之前缓存的代码
